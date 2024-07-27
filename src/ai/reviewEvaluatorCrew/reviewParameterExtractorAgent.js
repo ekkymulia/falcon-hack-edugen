a6c4b6671868cf7d6727f16d2e3b11305126f7f8
ekkymulia <ekky.mulialasardi@gmail.com>
2024-07-28 02:47:57 +0700

    add review longterm

---
+++ src/ai/reviewEvaluatorCrew/reviewParameterExtractorAgent.js
@@ -0,0 +1,47 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+
+/**
+ * Define your chat model to use.
+ */
+const model = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  temperature: 1,
+});
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", `Berikan hasil ekstraksi yang perlu diingat, dari kriteria yang diinputkan pada parameter dibawah ini
+    Point Kriteria:
+    {parameter}
+    
+    jika tidak ada menyebutkan salah satu kriteria diatas, isikan kriteria dengan data string kosong, tetpai key adalah parameter nama
+    berikan output dengan ditengahi oleh tanda garis | (cth:  'harga' | 'murah/mahal/sesuai', ....)
+    berikan hasil dalam lowercase dan tanpa tanda kutip
+  .`],
+  ["human", "{review}"],
+]);
+
+const runnableAgent = RunnableSequence.from([
+  prompt,
+  model,
+]);
+
+export const reviewParameterExtractorAgent = async (parameter, review) => {
+    console.log("Parameter:", parameter);
+    console.log("Review:", review);
+  
+    const result = await runnableAgent.invoke({ parameter, review });
+  
+    const parts = result.content.replace(/'/g, '').split(' | ');
+
+    // Step 2: Construct the JSON object
+    const jsonObject = [
+    parts[1],
+    parts[3],
+    parts[5],
+    parts[7]
+    ];
+
+    return jsonObject;
+};
---
+++ src/app/api/review/route.js
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+import { reviewEvaluatorCrew } from "@/ai/reviewEvaluatorCrew/crew";
+
+const prisma = new PrismaClient();
+
+const corsHeaders = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+export async function POST(request) {
+    try {
+        const data = await request.json();
+
+        const reviewEvaluator = await reviewEvaluatorCrew(data.review);
+
+        const review = await prisma.Review.create({
+            data: {
+                user_id: data.user_id,
+                bill_id: data.bill_id,
+                message: data.review,
+                rating: data.rating,
+            },
+        });
+
+        for (const [key, value] of Object.entries(reviewEvaluator.hasilEkstrasi)) {
+            try {
+                const param = JSON.parse(reviewEvaluator.paramObject)[key];
+
+                await prisma.LongtermReviewStore.create({
+                    data: {
+                        review_id: review.id ?? null,
+                        user_id: data.user_id ?? null,
+                        merchant_id: data.merchant_id ?? null,
+                        bill_id: data.bill_id ?? null,
+                        things_longterm_review_store_id: param.id ?? null,
+                        things_to_remember: value ?? null,
+                    },
+                });
+            } catch (error) {
+                console.error(`Error saving review for key ${key}:`, error);
+            }
+        }
+
+        return new Response(JSON.stringify(review), {
+            status: 200,
+            headers: corsHeaders,
+        });
+    } catch (error) {
+        console.error(error);
+        
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+            headers: corsHeaders,
+        });
+    }
+}
