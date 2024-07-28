2daf3a2be60346177d5cdbc4a781122e4835d745
RobertBenyamin <robertbenyamino@gmail.com>
2024-07-28 10:15:47 +0700

    feat: Add Next.js links to BottomNavigation component

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   House,
   FileClock,
@@ -9,26 +10,36 @@ import {
 const BottomNavigation = () => {
   return (
     <div className="bottom-0 bg-white relative w-full inset-x-0 py-2 px-4 flex flex-row justify-evenly border-t border-gray-200">
-      <div className="flex flex-col justify-center items-center">
-        <House size={24} />
-        <span className="text-xs">Beranda</span>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <FileClock size={24} />
-        <span className="text-xs">Aktivitas</span>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <CircleDollarSign size={24} />
-        <span className="text-xs">Pembayaran</span>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <MessageSquareText size={24} />
-        <span className="text-xs">Kotak Masuk</span>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <CircleUserRound size={24} />
-        <span className="text-xs">Akun</span>
-      </div>
+      <Link href="/home" passHref>
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <House size={24} />
+          <span className="text-xs">Beranda</span>
+        </div>
+      </Link>
+      <Link href="/activity" passHref>
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <FileClock size={24} />
+          <span className="text-xs">Aktivitas</span>
+        </div>
+      </Link>
+      <Link href="/payment" passHref>
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <CircleDollarSign size={24} />
+          <span className="text-xs">Pembayaran</span>
+        </div>
+      </Link>
+      <Link href="/inbox" passHref>
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <MessageSquareText size={24} />
+          <span className="text-xs">Kotak Masuk</span>
+        </div>
+      </Link>
+      <Link href="/account" passHref>
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <CircleUserRound size={24} />
+          <span className="text-xs">Akun</span>
+        </div>
+      </Link>
     </div>
   );
 };
